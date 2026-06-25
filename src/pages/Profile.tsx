1dd6873cee9fca4b17dd9a50f28c0ed1fe8a828f
mjblessent <163654675+mjblessent@users.noreply.github.com>
2026-06-25 19:57:25 -0400

    Undid formating

--- src/pages/Profile.tsx
+++ src/pages/Profile.tsx
@@ -12,7 +12,7 @@ const Profile = () => {
 
     return( <div className="max-w-4xl mx-auto ">
         <Navbar />
-        <h1 className="text-4xl font-bold text-center my-8">Profile Page!</h1>
+        <h1>Profile Page!</h1>
         <GetProfile/>
         <Footer/>
     </div>

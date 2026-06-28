7f50973742e7a9cbf8e2569926ffceea597455c1
mjblessent <163654675+mjblessent@users.noreply.github.com>
2026-06-27 20:49:56 -0400

    Final dependency update

--- vite.config.ts
+++ vite.config.ts
@@ -8,9 +8,13 @@ export default defineConfig({
   build: {
     rollupOptions: {
       output: {
-        manualChunks: {
-          vendor: ['react', 'react-dom', 'react-router-dom'],
-          firebase: ['firebase/app', 'firebase/auth', 'firebase/firestore', 'firebase/storage']
+        manualChunks: (id) => {
+          if (id.includes('node_modules/react/') || id.includes('node_modules/react-dom/') || id.includes('node_modules/react-router-dom/')) {
+            return 'vendor';
+          }
+          if (id.includes('node_modules/firebase/') || id.includes('node_modules/@firebase/')) {
+            return 'firebase';
+          }
         }
       }
     },
